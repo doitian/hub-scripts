afa5f3c5fe9ed5af24cdefbe17149e63280bcb81
ian <me@iany.me>
2018-12-27 19:23:23 +0800

    import/export labels

---
+++ export-labels.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+require 'dotenv/load'
+require 'octokit'
+require 'json'
+
+if ARGV.size == 0 then
+  $stderr.puts 'export-labels.rb user/repo'
+  exit 1
+end
+
+project = ARGV[0]
+
+out_dir = "out/#{project}"
+FileUtils.mkdir_p(out_dir)
+
+preview_header = { accept: 'application/vnd.github.symmetra-preview+json' }
+client = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
+client.auto_paginate = true
+labels = client.labels(project, preview_header).map do |label|
+  {
+    name: label.name,
+    color: label.color,
+    description: label.description
+  }
+end
+
+File.open("#{out_dir}/labels.json", 'w') do |fd|
+  fd.write(JSON.pretty_generate(labels))
+end
---
+++ import-labels.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+require 'dotenv/load'
+require 'octokit'
+require 'json'
+
+if ARGV.size != 2 then
+  $stderr.puts 'import-labels.rb user/repo issues.json'
+  exit 1
+end
+
+project, path = ARGV
+
+labels_to_add = {}
+File.open(path, 'r') do |fd|
+  JSON.parse(fd.read).each do |label|
+    labels_to_add[label['name']] = {
+      color: label['color'],
+      description: label['description']
+    }
+  end
+end
+
+preview_header = { accept: 'application/vnd.github.symmetra-preview+json' }
+client = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
+client.auto_paginate = true
+
+client.labels(project, preview_header).each do |label|
+  label_to_update = labels_to_add.delete(label[:name])
+  if label_to_update && (label_to_update[:color] != label[:color] || label_to_update[:description] != label[:description]) then
+    puts "update #{label[:name]}"
+    client.update_label(project, label[:name], label_to_update.merge(preview_header))
+  end
+end
+
+labels_to_add.each do |(name, label)|
+  extra = preview_header.merge(description: label[:description])
+  puts "add #{name}"
+  client.add_label(project, name, label[:color], extra)
+end
+
