82cc3fd5a03243307295b7b9ce773101fadb7252
ian <me@iany.me>
2019-01-10 20:06:26 +0800

    allow using issue id in batch-cards.rb

--- batch-cards.rb
+++ batch-cards.rb
@@ -17,7 +17,12 @@ File.open(path, 'r') do |fd|
   JSON.parse(fd.read).each_pair do |(col_id, notes)|
     if col_id.to_i.to_s == col_id
       notes.each do |note|
-        client.create_project_card(col_id.to_i, preview_header.merge(note: note))
+        puts "create: #{note.lines.first}"
+        if note.to_i.to_s == note then
+          client.create_project_card(col_id.to_i, preview_header.merge(content_id: note.to_i, content_type: 'Issue'))
+        else
+          client.create_project_card(col_id.to_i, preview_header.merge(note: note))
+        end
       end
     end
   end
